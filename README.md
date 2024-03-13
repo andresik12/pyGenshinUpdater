ec44f7c31e3b9a4377e1f24c601f53e58e29636d
Andrey <29600238+andresik12@users.noreply.github.com>
2024-03-13 17:15:25 +0300

    spell checking & writing README

--- README.md
+++ README.md
@@ -1,14 +1,72 @@
 # pyGenshinUpdater
-PC "Genshin Impact" updater, written in Python
+"Genshin Impact" PC version updater, written in Python
 
-Script, that allow to bypass original Genshin impact launcer patching process, in case when you playing on  unsupported platform, like macOS or Linux. 
-Allows mannualy specify the path to unpacked patch arhive, even when patch on another drive.
+Script, that allow to bypass original Genshin impact launcher patching process, in case when you playing on unsupported platform, like macOS or Linux, or you just don't like endless list of electron app on your pc. 
+Allows manually specify the path to unpacked patch archive, even when patch folder on another drive.
 
 ## Requirements:
-	1. >=Python3.11
-	1. Unziped patch files (you can download zip's manually from mihoyo servers, just google "direct download links for genshin impact")
-	2. hpatchz execution file. You can find files here https://github.com/sisong/HDiffPatch/releases. You need to choose zip, depend from your OS (look at file name). This is same program, that mihoyo used in genshin (and in another products, probably), but they pre-compiled it in there execution, so we can't use from game dir.
-	3. Make sure, that you have enough free space for patched game, because script can't do it (rn)
+`Python 3.11.0` or higher <br />
+Download `main.py` from repo <br />
+Unzipped patch files. <br />
+hpatchz execution file. You can find files on their [GitHub releases page](https://www.github.com/sisong/HDiffPatch/releases/). <br />
 
-## Instruction
-	1. Make sure, that python installed. type in cmd or in your terminal "python --version", you should get somthing like that: "Python *.*.*". Make sure version equal or greater that 3.11.0
+Make sure, that you have **enough free space** for patched game, because script can't do it.  <br />
+
+- Where you can download patch zip? You can download zip's manually from mihoyo servers, just google "direct download links for genshin impact".  <br />
+	When you will downloading patch's zip's, pay attention to version. Mihoyo writes version in zip file name, like "game_4.0.0_4.1.0_hdiff", where 4.0.0 is version, that you have on your device and 4.1.0 is version, which will be the result of patching process. Usually, they provide for downloading 2 version below. Example: current version 4.3.0 - mihoyo api will provide "game_4.2.0_4.3.0_hdiff" and "game_4.1.0_4.3.0_hdiff", so you can update to current version from 4.2.0 and 4.1.0 etc.
+
+- What is hpatchz? This is same program, that mihoyo used in genshin to create audio banks file (and in another products, probably), but they pre-compiled it in there execution, so we can't use from game dir.  <br />
+
+- Where i need to place unzipped patch files? **Where you want.** I tested it by putting it on an external drive, works flawless. I don't recommend use network folders on Windows, macOS and Linux users can try, but i'm not tested it. <br />
+
+- Where i can find, what version of game installed right now? In "Genshin impact game" folder you have `config.ini` file (open it by text editing tool), that contain `game_version=` line, where number after `=` is version of your game. If you want start game with launcher, you need to edit this number, after patching process, otherwise game launcher will try to update your game<br />
+
+
+
+# Instructions
+
+## Windows
+Make sure, that python installed. type in cmd `python --version`, you should get something like that: `Python *.*.*`. Make sure version equal or greater that **3.11.0**. <br />
+If you don't have installed python, you can download and install from their [official site](https://www.python.org/downloads/). <br />
+Place `main.py` in any directory with `Tools` folder, that contain `hpatchz.exe` <br />
+
+Your folder should look like that:
+- /your dir
+	- main.py
+	- /Tools
+		- hpatchz.exe <br />
+
+After that, open `cmd` and change your directory by `cd` command to that folder. Try to execute the script by `python main.py`, you should get message with list of commands. To select something - enter function number and press **Enter**. <br />
+After selecting function, you will be asked about path to game folder and patch folder. Simply drag and drop folder to `cmd` window and press **Enter**. <br />
+
+## macOS
+Make sure, that python installed. type in Terminal (cmd+space, type "terminal" and press enter) `python --version`, you should get something like that: `Python *.*.*`. If you don't have python or version lower, install it via [brew](https://brew.sh/) or from [official site](https://www.python.org/downloads/). <br />
+Place `main.py` in any directory with `Tools` folder, that contain `hpatchz`. For the first run, you need to manually execute `hpatchz` by right click (click with two fingers) and press open, otherwise macOS don't allow execute it by security reason. It will launch terminal window and quickly closed by it self - that's fine.<br />
+
+Your folder should look like that:
+- /your dir
+	- main.py
+	- /Tools
+		- hpatchz <br />
+
+After that, open `Terminal` and change your directory by `cd` command to that folder. You can type `cd`, press **space** and drop folder, where main.py and Tools/ are located. Try to execute the script by `python main.py`, you should get message with list of commands. To select something - enter function number and press **Enter**. <br />
+After selecting function, you will be asked about path to game folder and patch folder. Simply drag and drop folder to `cmd` window and press **Enter**. <br />
+
+## Linux
+If you here for instruction, i don't know what to say /kappa/. You can read instruction for macOS, it's pretty much the same.
+
+# Commands
+
+**First command: Old game file cleaning** - execute it first, if you want patch you game. It's take list of files, that can be removed from game patch folder. You need enter path "Genshin impact game" folder and after that path to unzipped patch folder "game_". After execution you will get successful message and question, what do next. Press enter to return to beginning. <br />
+
+**Second command: Game files update** - select once, after successful first command. If you run the command after the first one, you will not be asked about the paths, otherwise, you will need to specify path to "Genshin impact game" folder and patch folder. For example, you can execute first command, close script and open again. <br />
+
+**Third command: Audio localization files update** - updating your localization pack. One pack per execution. At first execution, you will be asked about path to "Genshin impact game" folder and audio patch folder. After path, script will be ask you, what language you want to update. Pay attention, audio path must match with selected language, otherwise it will cause data corruption. Audio patch's will be called by different names: English audio patch will starts by "en-us_", Chinese - "zh-cn_", Japanese - "ja-jp_", Korean - "ko-kr_". If you have more than one sound pack, you need to update each in any order. <br />
+
+After patching, you need manually change version of your game in "Genshin impact game" folder at `config.ini` file. You can do it by any text editing tool. Edit `game_version=` line to patch version.
+
+Tested on:
+
+	macOSx64 13.4.1 with python 3.11.8
+	Windows 10 with python 3.11.8
+	steamOS 3.5.7 (archlinux) and python 3.11.3
--- main.py
+++ main.py
@@ -12,14 +12,14 @@ DIR_GAME_AUDIOASSETS = "GenshinImpact_Data/StreamingAssets/AudioAssets"
 DIR_GAME_FILES_UPDATE = ""
 DIR_AUDIO_LOCALIZATION_FILES_UPDATE = ""
 LANG_DICTIONARY=["English(US)", "Japanese", "Chinese", "Korean"]
-EXEC_EXTENTION = ""
+EXEC_EXTENSION = ""
 DIR_SLASH = "/"
 if os.name == 'nt':
-	EXEC_EXTENTION = ".exe"
+	EXEC_EXTENSION = ".exe"
 	DIR_SLASH = "\\"
 
 # Searching for files, that will be modified
-# optimization auido list for prevent checking checksum of files, that not be modified
+# optimization audio list for prevent checking checksum of files, that not be modified
 def dir_scan(root_dir: Path):
 	file_list = []
 	for (dirpath, dirnames, filenames) in os.walk(root_dir):
@@ -40,12 +40,12 @@ def audio_file_finder(hdiff_audio: list, audio_checksum: list):
 		for checksum in audio_checksum:
 			if checksum['FileName'] == hdiff_file_stem:
 				audio_list.append(checksum)
-			#TODO What happend if audio pck file wouldn't exist, but hdiff will be?
+			#TODO What happened if audio pck file wouldn't exist, but hdiff will be?
 	return audio_list
 
 # Opening checksum file and returning list of dict
 # filled by file name and md5 checksum
-def reading_and_formating_checksum_file(checksum_file_path: Path):
+def reading_and_formatting_checksum_file(checksum_file_path: Path):
 	checksum_list: list[dict]= []
 	with open(checksum_file_path, "r") as audio_checksum_file:
 		while True:
@@ -56,7 +56,7 @@ def reading_and_formating_checksum_file(checksum_file_path: Path):
 			checksum_list.append({'FileName': os.path.basename(line_to_dict['remoteName']), 'md5': line_to_dict['md5']})
 	return checksum_list
 
-# Returning list, that containt filtered checksum list by suffix
+# Returning list, that contains filtered checksum list by suffix
 def checksum_files_filter(raw_checksum_list: list, suff: str):
 	checksum_list: list[dict] = []
 	checksum_list = list(filter(lambda name: Path(name['FileName']).suffix == suff, raw_checksum_list))
@@ -71,7 +71,7 @@ def files_checksum_checker(checksum_to_compare: str, file_path: Path):
 		print('Checksum of', file_info[1], 'valid', sep=" ")
 		return True
 	else:
-		print('Somthing wrong with', file_info[1], 'at', file_info[0], sep=" ")
+		print('Something wrong with', file_info[1], 'at', file_info[0], sep=" ")
 		return False
 
 def audio_files_checksum_processing(hdiff_files_list: list, checksum_list: list, path_audio: Path):
@@ -81,7 +81,7 @@ def audio_files_checksum_processing(hdiff_files_list: list, checksum_list: list,
 		if not files_checksum_checker(file['md5'], Path.joinpath(path_audio, file['FileName'])):
 			print('Checksum of', file['FileName'], "didn't match \n")
 			print('Continuation may cause corruption and you will have to download the full audio archive')
-			answer = input('Continue? Enter "no" if you want to abort: ')
+			answer = input('Continue? Enter "no" if you want to abort: ') or "yes"
 			if answer[0].lower() == 'n':
 				sys.exit(1)
 
@@ -90,7 +90,7 @@ def audio_assets_patching(original_files_dir: Path, patch_files_dir: Path, patch
 	if Path(temp_folder_dir).is_dir():
 		shutil.rmtree(temp_folder_dir)		
 	os.mkdir(temp_folder_dir)
-	hpatch_exec = Path.joinpath(Path(TOOLS_DIR).resolve(), "hpatchz" + EXEC_EXTENTION) #add executable extension for NT
+	hpatch_exec = Path.joinpath(Path(TOOLS_DIR).resolve(), "hpatchz" + EXEC_EXTENSION) #add executable extension for NT
 	patch_dir_abs = Path.joinpath(patch_files_dir, patch_dir)
 	for file_name in file_list:
 		# Get original file name without .hdiff extension
@@ -98,13 +98,13 @@ def audio_assets_patching(original_files_dir: Path, patch_files_dir: Path, patch
 		# Searching for checksum of .pck file
 		checksum_pck = next(checksum for checksum in checksum_list if checksum['FileName'] == file_name_stem)
 		path_of_patched_pck = Path.joinpath(temp_folder_dir, file_name_stem)
-		subprocess.call([hpatch_exec, Path.joinpath(original_files_dir, file_name_stem), Path.joinpath(patch_dir_abs, file_name), path_of_patched_pck])
+		subprocess.call([hpatch_exec, Path.joinpath(original_files_dir, file_name_stem), Path.joinpath(patch_dir_abs, file_name), path_of_patched_pck], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
 		#check for hpatch create patched pck
 		if path_of_patched_pck.exists():
-			print(str(file_name_stem), "patched, checking cheksum..", sep=" ")
+			print(str(file_name_stem), "patched, checking checksum..", sep=" ")
 			#checking checksum before replacing OG file
 			if files_checksum_checker(checksum_pck['md5'], path_of_patched_pck):
-				print("Replaceing old pck by patched pck..")
+				print("Replacing old pck by patched pck..")
 				#replace old audio pck with patched pck
 				file_replace(Path.joinpath(temp_folder_dir, file_name_stem), Path.joinpath(original_files_dir, file_name_stem))
 				print("Deleting hdiff ", file_name)
@@ -123,8 +123,8 @@ def audio_localization_package_updater(path_game_root: Path, path_update_root: P
 	
 	#prepare for checking checksum
 	checksum_file_name = "Audio_" + LANG_DICTIONARY[lang_id - 1] + "_pkg_version"
-	checksum_list_original = reading_and_formating_checksum_file(Path.joinpath(path_game_root, checksum_file_name))
-	checksum_list_patched = reading_and_formating_checksum_file(Path.joinpath(path_update_root, checksum_file_name))
+	checksum_list_original = reading_and_formatting_checksum_file(Path.joinpath(path_game_root, checksum_file_name))
+	checksum_list_patched = reading_and_formatting_checksum_file(Path.joinpath(path_update_root, checksum_file_name))
 	path_audio_hdiff = Path.joinpath(Path(DIR_GAME_AUDIOASSETS), LANG_DICTIONARY[lang_id - 1])
     # Get .hdiff files list at localization update folder
 	file_list = []
@@ -155,8 +155,8 @@ def game_audio_updater(path_game_root: Path, path_update_root: Path):
 	path_checksum_file_new = Path.joinpath(path_update_root, "pkg_version")
 	path_game_audioassets = Path.joinpath(path_game_root, DIR_GAME_AUDIOASSETS)
 	path_update_audioassets = Path.joinpath(path_update_root, DIR_GAME_AUDIOASSETS)
-	checksum_list_original = checksum_files_filter(reading_and_formating_checksum_file(path_checksum_file_current), ".pck")
-	checksum_list_patched = checksum_files_filter(reading_and_formating_checksum_file(path_checksum_file_new), ".pck")
+	checksum_list_original = checksum_files_filter(reading_and_formatting_checksum_file(path_checksum_file_current), ".pck")
+	checksum_list_patched = checksum_files_filter(reading_and_formatting_checksum_file(path_checksum_file_new), ".pck")
 	os.remove(Path.joinpath(path_update_root, 'hdifffiles.txt'))
 	file_list = []
 	for (dirpath, dirnames, filenames) in os.walk(path_update_audioassets):
@@ -205,8 +205,9 @@ def dir_Selector(case_type: int):
 		DIR_GAME_ROOT_PATH = path_corrector(DIR_GAME_ROOT_PATH)
 	match case_type:
 		case 0:
-			DIR_GAME_FILES_UPDATE = input('Enter path of directory with game update files. Should starts "game_4.4.0_4.5.0" \n')
-			DIR_GAME_FILES_UPDATE = path_corrector(DIR_GAME_FILES_UPDATE)
+			if len(DIR_GAME_FILES_UPDATE) < 1:
+				DIR_GAME_FILES_UPDATE = input('Enter path of directory with game update files. Should starts "game_4.4.0_4.5.0" \n')
+				DIR_GAME_FILES_UPDATE = path_corrector(DIR_GAME_FILES_UPDATE)
 		case 1:
 			DIR_AUDIO_LOCALIZATION_FILES_UPDATE = input('Enter path of directory with audio localization update. Should starts like "en-us_4.4.0_4.5.0" \n')
 			DIR_AUDIO_LOCALIZATION_FILES_UPDATE = path_corrector(DIR_AUDIO_LOCALIZATION_FILES_UPDATE)
@@ -224,15 +225,14 @@ def old_files_cleaning(game_root: Path, patch_root: Path):
 	ending_question()
 
 def ending_question():
-	answer = input('Do you want to continue execution? If not, enter "no" \n')
+	answer = input('Do you want to continue execution? If not, enter "no" \n') or "yes"
 	if answer[0].lower() == 'n':
 		sys.exit(1)
 	else:
 		func_selector()
 	
 def func_selector():
-	caseNumber = input("\n 1: Old game file cleaning \n 2: Game files update \n 3: Audio localization files update \n")
-	print('Type "4" to exit')
+	caseNumber = input("\n 1: Old game file cleaning \n 2: Game files update \n 3: Audio localization files update \n 4: exit \n")
 
 	match caseNumber:
 		case "1":
@@ -245,13 +245,15 @@ def func_selector():
 			dir_Selector(0)
 			game_audio_updater(Path(DIR_GAME_ROOT_PATH), Path(DIR_GAME_FILES_UPDATE))
 			game_files_updater(Path(DIR_GAME_ROOT_PATH), Path(DIR_GAME_FILES_UPDATE))
+		case "4":
+			sys.exit(1)
 		case _:
 			print("Nothing selected, exit")
 
 def main():
 	print("Created for non-commercial purposes by https://github.com/andresik12")
 	print("Genshin impact files patcher")
-	print("For entering path of directories, you can simply drag and drop folder at cmd window (for widnows), terminal window (for macos), konsole (for linux)")
+	print("For entering path of directories, you can simply drag and drop folder at cmd window (for windows), terminal window (for macos), konsole (for linux)")
 	print("Extra symbols will be removed automatically")
 	print("Enter number of function to execute:")
 	func_selector()
